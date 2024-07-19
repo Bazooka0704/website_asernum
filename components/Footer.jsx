ec45949e69416cb762df99a48a2acdd2e6adda94
Bazooka0704 <yannask16@gmail.com>
2024-07-19 15:12:20 +0000

    updates 2024-07-19

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,7 +2,7 @@ export default function Footer(){
     return(
     <footer className="footer absolute bottom-0 min-w-full">
         <div className="w-full mx-auto max-w-screen-xl p-4 text-center">
-        <span className="text-md text-white font-aeonik-medium">Copyright © 2024 Asernum
+        <span className="text-sm md:text-md text-white font-aeonik-medium">Copyright © 2024 Asernum
         </span>
         </div>
     </footer>
